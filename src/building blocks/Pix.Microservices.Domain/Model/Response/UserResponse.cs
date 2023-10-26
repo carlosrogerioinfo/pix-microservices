e85230b67a039559a55930fbe9a4cbff303a4a8e
Carlos Rogerio <carlosrogerio.info@gmail.com>
2023-10-25 22:35:12 -0300

    Remove field

--- src/building blocks/Pix.Microservices.Domain/Model/Response/UserResponse.cs
+++ src/building blocks/Pix.Microservices.Domain/Model/Response/UserResponse.cs
@@ -8,12 +8,10 @@ namespace Pix.Microservices.Domain.Http.Response
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public UserType UserTypeId { get; set; }
         public string UserType { get; set; }
         public bool Active { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? LastUpdatedAt { get; set; }
-
     }
 }
